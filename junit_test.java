cc751cfff9e3e5776e19d2fd7c0abb187bab81ff
Arnar <arnarleifs@github.com>
2015-10-02 14:39:32 +0000

    Setup templates for unit tests using Java - Package jUnit

---
+++ junit_test.java
@@ -0,0 +1,29 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+// A template class for doing jUnit tests in Java
+public class jUnit {
+	public static void main(String args[]) {
+
+	}
+
+	@Test 
+	public void firstTest() {
+		// Arrange
+
+		// Act
+
+		// Assert
+		Assert.assertTrue('some parameters');
+	}
+
+	@Test
+	public void secondTest() {
+		// Arrange
+
+		// Act
+
+		// Assert
+		Assert.assertTrue('some parameters');
+	}
+}
