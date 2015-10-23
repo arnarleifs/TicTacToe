a93f5c8cf72adb34d0fa11a504ac736176611b42
Arnar <arnarleifs@github.com>
2015-10-23 21:56:11 +0000

    Created a title border around the game screen

--- src/main/java/is/ru/tictactoe/GameFrame.java
+++ src/main/java/is/ru/tictactoe/GameFrame.java
@@ -3,6 +3,7 @@ import java.awt.*;
 import java.awt.event.*;
 import java.awt.geom.*;
 import javax.swing.*;
+import javax.swing.border.*;
 
 @SuppressWarnings("serial")
 public class GameFrame extends JFrame {
@@ -14,32 +15,38 @@ public class GameFrame extends JFrame {
 
 	public GameFrame() {
 		panel = new GamePanel();
-		this.add(panel);
 		panel.setPreferredSize(sizeOfPanel);
+		panel.setBorder(new TitledBorder(new LineBorder(Color.black, 5),
+        "TicTacToe!"));
 		panel.addMouseListener(new MouseAdapter() {
 			@Override
-			public void mouseClicked(MouseEvent e) {
+			public void mouseClicked(MouseEvent e) { 
+				// Check if clicked in a valid cell
+
 				System.out.println("X: " + e.getX());
 				System.out.println("Y: " + e.getY());
 				System.out.println("---------------");
+				repaint();
 			}
 		});
+		this.add(panel);
 		this.setSize(sizeOfFrame);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setTitle(TITLE);
 		this.setVisible(true);
 
+		// Container for the layout of the screen
 		Container container = this.getContentPane();
 		container.setLayout(new BorderLayout());
 		container.add(panel, BorderLayout.CENTER);
 	} 
 
 	public class GamePanel extends JPanel {
+		private int x = -50;
+		private int y = -50;
 		@Override
 		public void paintComponent(Graphics g) {
 			Graphics2D g2d = (Graphics2D) g;
-			g2d.setColor(Color.RED);
-			g2d.drawRect(0, 0, 20, 20);
 		}
 	}
 
