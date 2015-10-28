017b5b08e1230cb6eae910243ee060aef354e8cb
Arnar <arnarleifs@github.com>
2015-10-28 15:07:36 +0000

    Added compatibility with Heroku in MainWeb.java

--- src/main/java/is/ru/tictactoe/MainWeb.java
+++ src/main/java/is/ru/tictactoe/MainWeb.java
@@ -8,11 +8,21 @@ import java.util.HashMap;
 public class MainWeb {
     private static String layout = "templates/layout.vtl";
     public static void main(String[] args) {
+        port(getHerokuAssignedPort());
         staticFileLocation("/public");
         get("/", (req, res) -> {
             HashMap model = new HashMap();
             model.put("template", "templates/main.vtl");
             return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
+    }
+
+    // For compability reasons (Heroku)
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
     }
 }
