da4e24a3d253779d168e505dc6ccf82d00a5d1cc
Arnar <arnarleifs@github.com>
2015-10-31 01:13:30 +0000

    Changed code according to codestyle standards

--- src/main/java/is/ru/tictactoe/DBConnection.java
+++ src/main/java/is/ru/tictactoe/DBConnection.java
@@ -4,53 +4,60 @@ import java.io.*;
 import java.util.*;
 
 public class DBConnection {
-	private String userName = "tictactoe";
-	private String password = "ticTacTo3";
-	private String dbms = "sqlserver";
-	private String serverName = "tropaeolum.arvixe.com:1433";
-	private String dbName;
 
-	public DBConnection() {
-		initializeDatabaseName();
-	}
+    private String userName = "tictactoe";
+    private String password = "ticTacTo3";
+    private String dbms = "sqlserver";
+    private String serverName = "tropaeolum.arvixe.com:1433";
+    private String dbName;
 
-	public Connection getConnection() throws SQLException, ClassNotFoundException {
-	    Connection conn = null;
-	    Properties connectionProps = new Properties();
-	    connectionProps.put("user", this.userName);
-	    connectionProps.put("password", this.password);
-	    Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+    public DBConnection() {
+
+        initializeDatabaseName();
+    }
+
+    public Connection getConnection() throws SQLException, ClassNotFoundException {
+
+        Connection conn = null;
+        Properties connectionProps = new Properties();
+        connectionProps.put("user", this.userName);
+        connectionProps.put("password", this.password);
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
         conn = DriverManager.getConnection("jdbc:" + this.dbms + "://" +
                    this.serverName + ";DatabaseName=" + dbName, connectionProps);
-	    System.out.println("Connected to database");
-	    return conn;
-	}
+        System.out.println("Connected to database");
+        return conn;
+    }
 
-	// This is used to identify between the staging database and the production database
-	private void initializeDatabaseName() {
-		try {
-			BufferedReader br = new BufferedReader(new FileReader("connectionstring.txt"));
-		    StringBuilder sb = new StringBuilder();
-		    String line = br.readLine();
-		    while (line != null) {
-		        sb.append(line);
-		        sb.append(System.lineSeparator());
-		        line = br.readLine();
-		    }
-		    this.dbName = sb.toString();
-		} catch (Exception ex) {
-			System.out.println(ex.getMessage());
-		}
-	}
+    // This is used to identify between the staging database and the production database
+    private void initializeDatabaseName() {
 
-	public static void insertResultsToDb(Seed player, Connection dbConnection) {
+        try {
+            BufferedReader br = 
+            new BufferedReader(new FileReader("connectionstring.txt"));
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
+            while (line != null) {
+                sb.append(line);
+                sb.append(System.lineSeparator());
+                line = br.readLine();
+            }
+            this.dbName = sb.toString();
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public static void insertResultsToDb(Seed player, Connection dbConnection) {
+
         String winner = "";
         if (player == Seed.CROSS) { winner = "X_WON"; }
         else if (player == Seed.CIRCLE) { winner = "CIRCLE_WON"; }
         else { winner = "DRAW"; }
         try {
             Statement statement = dbConnection.createStatement();
-            statement.executeUpdate("INSERT INTO tictactoe.Scores (winner, date_of_win) VALUES('" + winner + "', GETDATE())");
+            statement.executeUpdate("INSERT INTO tictactoe.Scores " + 
+                                    " (winner, date_of_win) VALUES('" + winner + "', GETDATE())");
         } catch (SQLException ex) {
             System.out.println(ex.getMessage());
         }
--- src/test/java/is/ru/tictactoe/DatabaseConnectionJUnit.java
+++ src/test/java/is/ru/tictactoe/DatabaseConnectionJUnit.java
@@ -4,56 +4,57 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class DatabaseConnectionJUnit {
-	private DBConnection conn = new DBConnection();
+    private DBConnection conn = new DBConnection();
 
-	@Test
-	public void testDatabaseConnection() {
-		int returnValue = 1;
-		try {
-			Connection connection = conn.getConnection();
-		} catch (SQLException ex) {
-			assertEquals(0, returnValue);
-		} catch (ClassNotFoundException cex) {
-			assertEquals(0, returnValue);
-		}
-		assertEquals(1, returnValue);
-	}
+    @Test
+    public void testDatabaseConnection() {
+        int returnValue = 1;
+        try {
+            Connection connection = conn.getConnection();
+        } catch (SQLException ex) {
+            assertEquals(0, returnValue);
+        } catch (ClassNotFoundException cex) {
+            assertEquals(0, returnValue);
+        }
+        assertEquals(1, returnValue);
+    }
 
-	@Test
-	public void testGetFirstRowFromDatabase() {
-		String winner = "";
-		String query = "SELECT TOP 1 * FROM tictactoe.Scores ORDER BY ID ASC;";
-		try {
-			Connection connection = conn.getConnection();
-			Statement statement = connection.createStatement();
-			ResultSet rs = statement.executeQuery(query);
-			// Loops through the result set given from the query above
-			while (rs.next()) {
-				winner = rs.getString("winner");
-			}
-		} catch (Exception ex) {
-			assertEquals("NOT1337", winner);
-		}
-		assertEquals("X_WON", winner);
-	}
+    @Test
+    public void testGetFirstRowFromDatabase() {
+        String winner = "";
+        String query = "SELECT TOP 1 * FROM tictactoe.Scores ORDER BY ID ASC;";
+        try {
+            Connection connection = conn.getConnection();
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery(query);
+            // Loops through the result set given from the query above
+            while (rs.next()) {
+                winner = rs.getString("winner");
+            }
+        } catch (Exception ex) {
+            assertEquals("NOT1337", winner);
+        }
+        assertEquals("X_WON", winner);
+    }
 
-	@Test
-	public void testInsertToDatabase() {
-		String resultString = "";
-		String query = "SELECT TOP 1 * FROM tictactoe.Scores ORDER BY ID DESC";
-		try {
-			Connection connection = conn.getConnection();
-			DBConnection.insertResultsToDb(Seed.CROSS, connection);
-			Statement statement = connection.createStatement();
-			ResultSet rs = statement.executeQuery(query);
-			while(rs.next()) {
-				resultString = rs.getString("winner");
-			}
-			// Clean up afterwards
-			statement.executeUpdate("DELETE FROM tictactoe.Scores WHERE ID=(SELECT MAX(ID) FROM tictactoe.Scores)");
-		} catch (Exception ex) {
-			assertEquals("NOT1337", resultString);
-		}
-		assertEquals("X_WON", resultString);
-	}
+    @Test
+    public void testInsertToDatabase() {
+        String resultString = "";
+        String query = "SELECT TOP 1 * FROM tictactoe.Scores ORDER BY ID DESC";
+        try {
+            Connection connection = conn.getConnection();
+            DBConnection.insertResultsToDb(Seed.CROSS, connection);
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery(query);
+            while (rs.next()) {
+                resultString = rs.getString("winner");
+            }
+            // Clean up afterwards
+            statement.executeUpdate("DELETE FROM tictactoe.Scores " +
+                                    " WHERE ID=(SELECT MAX(ID) FROM tictactoe.Scores)");
+        } catch (Exception ex) {
+            assertEquals("NOT1337", resultString);
+        }
+        assertEquals("X_WON", resultString);
+    }
 }
