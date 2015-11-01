e0a591d769e3ce929f1b54c2db2f6e9d7c9b941b
Arnar <arnarleifs@github.com>
2015-11-01 14:31:47 +0000

    Python script for sending the latest jar to the FTP server completed and working

---
+++ bin/send_latest_jar.py
@@ -0,0 +1,17 @@
+import ftplib
+import os
+
+# Compile the latest .jar
+os.system("gradlew jar")
+
+# Upload file via FTP to server
+print("Starting to deploy latest .jar to FTP server")
+filename = "build/libs/TicTacToe.jar"
+ftp = ftplib.FTP('cptropaeolum.arvixe.com')
+ftp.login('tictactoe', 'ticTacTo3')
+ftp.cwd('checkthelunch.ekbjarnason.com/wwwroot/data')
+myfile = open(filename, 'rb')
+ftp.storbinary('STOR TicTacToe.jar', myfile)
+myfile.close()
+ftp.quit()
+print("Latest .jar upload to FTP server completed!")
