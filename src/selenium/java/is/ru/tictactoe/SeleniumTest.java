7089b750ebec3f3ee5f02c20a472e6a38c084557
Arnar <arnarleifs@github.com>
2015-11-02 13:28:08 +0000

    Added correct configurations for travis and local selenium

--- src/selenium/java/is/ru/tictactoe/SeleniumTest.java
+++ src/selenium/java/is/ru/tictactoe/SeleniumTest.java
@@ -49,7 +49,7 @@ public class SeleniumTest {
     @Test
     public void testTitle() {
         String title = driver.getTitle();
-        assertEquals("SUP", title);
+        assertEquals("Tic tac toe", title);
     }
 
     @AfterClass
